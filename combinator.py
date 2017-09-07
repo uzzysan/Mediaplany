4087271314fb883c58b9b050c50195156cd874ea
Rafał Maculewicz <r.maculewicz@teb-akademia.pl>
2017-09-07 16:15:34 +0200

    Ładuje komplet danych, poprawnie wybiera zakresy. Do zrobienia: - zachowanie kolejności kolumn(zmienia na A, AA, B, BB,...) - załadownanie wiersza nagłówka - usunięcie pustych wierszy przed zapisaniem do excela - ukrycie kolumn ukrytych w plikach źródłowych

--- combinator.py
+++ combinator.py
@@ -1,25 +1,39 @@
 #!/usr/bin/env python
 #Skrypt składający w jeden zbiorczy plik inormacje z mediaplanów w bieżącym katalogu
 
-import os, re
+import os
+import re
+
 from openpyxl import load_workbook, Workbook
-from openpyxl.utils import get_column_letter
-import numpy as np
+from openpyxl.utils import get_column_letter, get_column_interval
 import pandas as pd
-from pandas import Series, DataFrame
+from pandas import DataFrame
 
 def listMpFiles():
-    "Tworzy listę plików z poszczególnymi mediaplanami w bieżącym katalogu"
-    mpFiles = [f for f in os.listdir('.') if re.match(r'\b[a-zA-Z0-9].*.xls*', f)]
-    numFiles = len(mpFiles)
-    print('Znalazłem {} plików.'.format(numFiles))
-    return mpFiles
+    """Tworzy listę plików z poszczególnymi mediaplanami w bieżącym katalogu"""
+    mp_files = [f for f in os.listdir('.') if re.match(r'\b[a-zA-Z0-9].*.xls*', f)]
+    num_files = len(mp_files)
+    print('Znalazłem {} plików.'.format(num_files))
+    return mp_files
+
+def sheetToDataFrame(range_string, ws, kolumna):
+    """Tworzy DataFrame z zakresu komórek 'A4':podana ostatnia kolumna i wiersz danych"""
+    data_rows = []
+    for row in ws[range_string]:
+        data_rows.append([cell.value for cell in row])
+
+    return DataFrame(data_rows, columns = get_column_interval('A', kolumna))
 
 #if __name__ == __main__:
 
 mediaplany = listMpFiles()
+baza = Workbook()
+arkuszBazy = baza.active
+currentSheetDF = DataFrame()
+finalDF = DataFrame()
 
-arkusze = ('LIC - MP', 'LIC - H', 'SUM - MP', 'SUM - H', 'SP - MP', 'SP - H', 'MBA - MP', 'MBA - H', 'Szkolenia - MP', 'Ogólne',)
+arkusze = ('LIC - MP', 'LIC - H', 'SUM - MP', 'SUM - H', 'SP - MP', 'SP - H', 'MBA - MP', 'MBA - H',
+           'Szkolenia - MP', 'Ogólne',)
 
 #pętla ładująca poszczególne pliki z mediaplanami i sprawdzająca czy zawierają potrzebne arkusze
 for filename in mediaplany:
@@ -27,7 +41,7 @@ for filename in mediaplany:
         wb = load_workbook(filename, keep_vba=True)
     except:
         print('Błąd otwarcia pliku: {}'.format(filename))
-      
+
     print('Sprawdzam plik: {}'.format(filename))
 
     for arkusz in arkusze: #ładuje poszczególne arkusze i zbiera z nich dane
@@ -60,10 +74,20 @@ for filename in mediaplany:
                 kolumna = get_column_letter(sumaInCol)
                 print('\tOstatnia kolumna danych to: {}'.format(kolumna))
 
-        wiersz = 0
-        for row in ws.iter_rows(min_row=4, max_col=113, max_row=lastRow):
-            wiersz += 1
-        print('\tArkusz {}, wczytano {} wierszy danych.'.format(arkusz, wiersz))
+        final_data_cell = str(kolumna) + str(lastRow)
+        cellsRange = 'A4' + ':' + str(final_data_cell)
+        currentSheetDF = sheetToDataFrame(cellsRange, ws, kolumna)
+        finalDF = finalDF.append(currentSheetDF, ignore_index=True)
 
-    print('Plik: {} OK!'.format( filename))
+    print('Plik: {} OK!'.format(filename))
+
+writer = pd.ExcelWriter('baza.xlsx', engine='xlsxwriter')
+
+finalDF.to_excel(writer,
+                 sheet_name='Baza',
+                 index=False)
+
+workbook = writer.book
+worksheet = writer.sheets['Baza']
+writer.save()
 
